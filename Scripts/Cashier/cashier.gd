ce1cf85d783b7562a40127549c693194f86aebd7
Khubayan <kubayan73@gmail.com>
2025-05-02 14:10:23 +0800

    Prepare Orders

--- Scripts/Cashier/cashier.gd
+++ Scripts/Cashier/cashier.gd
@@ -5,17 +5,21 @@ class_name Cashier
 
 @export var move_speed := 50.0
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
+@onready var cook_bar: CookBar = $CookBar
 
 var current_customer: Customer
 var counter_pos: Vector2
 
-
+var item_request: Item
+var item_counter_pos: Vector2
 
 
 func set_customer(customer: Customer) -> void:
 	current_customer = customer
 	customer.being_served = true
 	counter_pos = Vector2(customer.position.x, customer.position.y + 160)
+	item_request = customer.request_item
+	item_counter_pos = GameManager.get_item_counter_pos(item_request)
 
 func take_order() -> void:
 	move_to_customer()
@@ -31,7 +35,23 @@ func  move_to_customer() -> void:
 	anim_player.play("move")
 
 func move_to_item_position() -> void:
+	anim_player.play("idle")
 	# Create tween
+	var tween := create_tween()
+	tween.tween_property(self, "position", item_counter_pos, 1.0)
+	tween.tween_interval(0.5)
+	tween.finished.connect(func(): start_cook_time())
 	# Move to item counter pos
 	# Start cook time
-	anim_player.play("idle")
+
+func start_cook_time() -> void:
+	cook_bar.show()
+	cook_bar.cook_item(item_request.cook_time)
+
+func delivered_order() -> void:
+	print("Cook completed")
+
+func _on_cook_bar_on_cook_completed() -> void:
+	cook_bar.hide()
+	cook_bar.reset_bar()
+	delivered_order()
--- autoloads/game_manager.gd
+++ autoloads/game_manager.gd
@@ -5,6 +5,17 @@ signal on_customer_request(customer: Customer)
 const ITEM_BURGER = preload("res://Data/item_burger.tres")
 const ITEM_COFFE = preload("res://Data/item_coffe.tres")
 
+var coffee_counter_pos := Vector2(415, 1250)
+var burger_counter_pos := Vector2(665, 1250)
+
 func get_random_item() -> Item:
 	var items: Array = [ITEM_BURGER, ITEM_COFFE]
 	return items.pick_random()
+
+func get_item_counter_pos(item: Item) -> Vector2:
+	match item.type:
+		Item.ItemType.Coffee:
+			return coffee_counter_pos
+		Item.ItemType.Burger:
+			return burger_counter_pos
+	return Vector2.ZERO
