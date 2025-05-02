372d1025fb710b0bb96783f1a9407a0f227184e6
Khubayan <kubayan73@gmail.com>
2025-05-02 10:43:44 +0800

    Move Customer to Counter

--- Scripts/Customer/customer.gd
+++ Scripts/Customer/customer.gd
@@ -14,11 +14,23 @@ var request_item: Item
 var request_quantity: int
 var current_order_status: int
 
+var counter_pos: Vector2
+
 func init_customer(item: Item, quantity: int) -> void:
 	request_item = item
 	request_quantity = quantity
 	current_order_status = quantity
 	show_order_ui()
+
+func move_to_counter() -> void:
+	play_move_anim()
+	var tween :Tween = create_tween()
+	tween.tween_property(self, "position", Vector2(counter_pos.x, position.y), 1.0)
+	tween.tween_interval(0.2)
+	tween.tween_property(self, "position", counter_pos, 1.0)
+	tween.tween_interval(0.5)
+	tween.finished.connect(func(): anim_player.play("idle"))
+	
 
 func set_sprites(data: CustomerData) -> void:
 	body.texture = data.body
@@ -33,4 +45,3 @@ func show_order_ui() -> void:
 	item_box.show()
 	item_img.texture = request_item.sprite
 	item_label.text = str(request_quantity)
-	
--- Scripts/Managers/customer_manager.gd
+++ Scripts/Managers/customer_manager.gd
@@ -5,6 +5,7 @@ class_name CustomerManager
 @export var spawn_positions: Array[Marker2D]
 @export var customer_sprites: Array[CustomerData]
 @export var customer_scene: PackedScene
+@onready var counter_manager: CounterManager = %CounterManager
 
 
 
@@ -19,14 +20,20 @@ func spawn_customer() -> void:
 	var random_item: Item = GameManager.get_random_item()
 	var random_quantity: int = randi_range(1, 3)
 	customer_instance.init_customer(random_item, random_quantity)
-	#Get random spawn position
-	var random_spawn_pos: Marker2D = spawn_positions.pick_random()
-	customer_instance.position = random_spawn_pos.position
-	customer_instance.play_move_anim()
-	#Move customer
-	var tween: Tween = create_tween()
-	tween.tween_property(customer_instance, "position", customer_instance.position + Vector2.RIGHT * 1350, 5.0)
-	tween.finished.connect(func(): customer_instance.queue_free())
+	
+	if counter_manager.get_free_index() != -1:
+		customer_instance.position = spawn_positions[1].position
+		counter_manager.assign_customer(customer_instance)
+		customer_instance.move_to_counter()
+	else:
+		#Get random spawn position
+		var random_spawn_pos: Marker2D = spawn_positions.pick_random()
+		customer_instance.position = random_spawn_pos.position
+
+		#Move customer
+		var tween: Tween = create_tween()
+		tween.tween_property(customer_instance, "position", customer_instance.position + Vector2.RIGHT * 1350, 5.0)
+		tween.finished.connect(func(): customer_instance.queue_free())
 
 
 func _on_spawn_timer_timeout() -> void:
